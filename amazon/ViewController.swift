d2698f64620fdacb0c4be0ea1b9e7bbcdabfc12e
O'Brien, Patrick (Synchrony Financial) <212464337@ussyfstml203329.local>
2016-07-18 14:14:43 -0400

    Initial Commit

---
+++ amazon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  amazon
+//
+//  Created by O'Brien, Patrick (Synchrony Financial) on 7/18/16.
+//  Copyright © 2016 O'Brien, Patrick (Synchrony Financial). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
